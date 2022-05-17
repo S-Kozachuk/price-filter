e78b14ab899e61be9150676e7ab0223151f89e5a
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-05-17 14:38:31 +0300

    Changed the order in array a

--- js/filter.js
+++ js/filter.js
@@ -1,19 +1,11 @@
 // Sorting array
-let a  = [
-	3,
-	-14,
-	0,
-	1,
-	5,
-	-7,
-	9
-]
+let a  = [3,-14,0,1,5,-7,9]
 
 // Empty array
 let b =[];
 
 // Cycle for sorting 
-for (let i=2; i<a.length; i++) {
+for (let i=0; i<a.length; i++) {
 	if (a[i]>=1) b.push(a[i]);
 }
 

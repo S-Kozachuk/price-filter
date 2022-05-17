319135d590e3a36935e14771327703a830388ef2
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-05-17 16:13:54 +0300

    Added the empty filter

--- js/filter.js
+++ js/filter.js
@@ -1,13 +1,19 @@
 // Sorting array
-let a  = [3,-14,0,1,5,-7,9]
+let a = [-10,3,-14,0,1,5,-7,9]
 
 // Empty array
 let b =[];
 
 // Cycle for sorting 
 for (let i=0; i<a.length; i++) {
-	if (a[i]>=1) b.push(a[i]);
+	if (a[i]>=0) b.push(a[i]);
 }
 
 console.log(a);
 console.log(b);
+
+let c = a.filter(function() {
+
+});
+
+console.log(c);

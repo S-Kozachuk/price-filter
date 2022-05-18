6fb29a3437a3dba5d583c926bbe76523abea54d6
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-05-18 15:51:33 +0300

    Added new fuction

--- js/main.js
+++ js/main.js
@@ -20,15 +20,49 @@ console.log(courses[3].name [11]);
 console.log(courses[0].prices[0]);
 
 // Вывод значений вложенного массива (доступ к ценам)
-console.log(courses[2]. prices)
-
+console.log(courses[3]. prices)
+// Get Italy price in variable
+let italy = courses[2].prices
 
 // Варианты цен (фильтры), которые ищет пользователь
 let requiredRange1 = [null, 200];
 let requiredRange2 = [100, 350];
 let requiredRange3 = [200, null];
 
+// Сравнить значения Италии с  первым фильтром italy == requiredRange1
+/*
+function courseSearch(){
+	if italy == requiredRange1
+}
+*/
+
+// Search function
+/*
+let filterResult = courses.filter(function (currentValue){
+	return currentValue = BiquadFilterNode;
+	If courses[2]
+});
+console.log(filterResult)
+*/
+
+//Вывести в консоль значения фильтров 
+// Первый параметр фильтра (1-ое значение массива)
+console.log(requiredRange1 [0])
+// Параметры фильтра (значения массива)
+console.log(requiredRange2)
 
 
+// Мысли
+/*
+Вместо currentValue нужна запись, обеспечивающая доступ к массиву
+внутри объекта. Далее эту функции нужно вложить в цикл for для проверки
+всех объектов на соответвие фильтру внутри массива courses.
 
-//let result = courses.filter()
+В качестве результата нужно вывести не значение (цена) свойства prices
+(вложенный массив), а название товара (значение свойства name).
+
+Какой метод применить для получения доступа к свойству prices?
+Нужно сравнивать только одинаковые сущности. 
+Массив (внутри courses) с диапазоном цен 
+расположен на уровень ниже, чем массив, представляющий фильтр.
+*/

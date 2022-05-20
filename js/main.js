7f4e40ca17c3d381469f0ecb4d109fbe3030d0c5
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-05-20 12:32:16 +0300

    Added 2-nd variant to bypass of array

--- js/main.js
+++ js/main.js
@@ -10,52 +10,31 @@ let courses = [
 	{ name: "Courses in France", prices: [null, null] },
 ];
 
-// Вызов значений объекта
-result = Object.keys(courses);
-	// Получение значения name
+// Получение значения name
 console.log(courses[3].name);
-	// Получение отдельного элемента значения по №
-console.log(courses[3].name [11]);
-	// Доступ к значению вложенного массива
-console.log(courses[0].prices[0]);
 
 // Вывод значений вложенного массива (доступ к ценам)
-console.log(courses[3]. prices)
-// Get Italy price in variable
-let italy = courses[2].prices
+console.log(courses[6].prices)
 
 // Варианты цен (фильтры), которые ищет пользователь
 let requiredRange1 = [null, 200];
 let requiredRange2 = [100, 350];
 let requiredRange3 = [200, null];
 
-// Сравнить значения Италии с  первым фильтром italy == requiredRange1
+
+// First variant (not work)
 /*
-function courseSearch(){
-	if italy == requiredRange1
-}
-*/
-
-// Search function
-/*
-let filterResult = courses.filter(function (currentValue){
-	return currentValue = BiquadFilterNode;
-	If courses[2]
-});
-console.log(filterResult)
-*/
-
-//Вывести в консоль значения фильтров 
-// Первый параметр фильтра (1-ое значение массива)
-console.log(requiredRange1 [0])
-// Параметры фильтра (значения массива)
-console.log(requiredRange2)
-searchResult = [];
-
 for (let i = 0; i <= courses; i++) {
-	if (requiredRange1 == courses[i]){
-		searchResult = courses[i]
+	if (requiredRange1 == courses[i].prices){
+		searchResult = courses[i].prices
 	}
 }
-
 console.log(searchResult);
+*/
+
+// Second variant
+for (let value of courses.values()) {
+	console.log(value);
+}
+
+ 

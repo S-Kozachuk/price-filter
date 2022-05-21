0cdbda677071b791377368615d6f2f4f7a1b5028
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-05-21 22:15:20 +0300

    Added the filter() method

--- js/filter.js
+++ js/filter.js
@@ -3,25 +3,33 @@ let a = [-10,3,-14,0,1,5,-7,9,28,6,11]
 
 // Classic method
 // Cycle for sorting  
-/*
+
 // Empty array
 let b =[];
 
 for (let i=0; i<a.length; i++) {
 	if (a[i]>=0) b.push(a[i]);
 }
-*/
+
+let c = a.filter(function() {
+	return true
+});
+
+
+
 // Filter method (only even)
 /*
 let b = a.filter(function (currentValue) {
 	return currentValue%2===0
 });
 */
-
 // With arrow function
+/*
 let b = a.filter(currentValue => {
 	return currentValue%2===0
 });
+*/
 
 console.log(a);
 console.log(b);
+console.log(c)

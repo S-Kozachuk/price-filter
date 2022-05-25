a98e72e0bdb5460b594ca3327d5f3a2c08e56c04
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-05-25 17:43:02 +0300

    Continue to understand in arrays intersect lesson

--- js/arrays-intresect.js
+++ js/arrays-intresect.js
@@ -1,12 +1,12 @@
-const input1 = [1,2,2,1,1,1,1];
-const input2 = [2,2,1,1,1];
+const input1 = [1,2,2,1];
+const input2 = [2,2];
 
-const input3 = [4,9,5,9,7];
-const input4 = [1,9,4,9,8,4,3,9];
+const input3 = [4,9,5];
+const input4 = [9,4,9,8,4];
 
 const intersect = function (nums1, nums2) {
 	let resultArray = [];
-	// Get a count of occurences in map 
+	// Get a count of occurences in map (from nums1)
 	let map = nums1.reduce((acc,i)=>{
 		acc[i] = acc[i] ? acc[i] + 1 : 1;
 		return acc;
@@ -14,6 +14,7 @@ const intersect = function (nums1, nums2) {
 
 	console.log(map);
 
+	// Search the intersections in 2-nd array (nums2)
 	for (let i = 0; i < nums2.length; i++) {
 		const current = nums2[i];
 		let count = map[current];

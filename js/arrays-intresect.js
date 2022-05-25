1354632fab641685d24432775b51e7b8dc2b1b53
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-05-25 14:45:01 +0300

    Added the comments to first array function

--- js/arrays-intresect.js
+++ js/arrays-intresect.js
@@ -1,18 +1,18 @@
 const input1 = [1,2,2,1,1,1,1];
 const input2 = [2,2,1,1,1];
 
-const input3 = [4,9,5];
-const input4 = [9,4,9,8,4];
+const input3 = [4,9,5,9,7];
+const input4 = [1,9,4,9,8,4,3,9];
 
 const intersect = function (nums1, nums2) {
 	let resultArray = [];
-
+	// Get a count of occurences in map 
 	let map = nums1.reduce((acc,i)=>{
 		acc[i] = acc[i] ? acc[i] + 1 : 1;
 		return acc;
 	}, {})
 
-	//console.log(map);
+	console.log(map);
 
 	for (let i = 0; i < nums2.length; i++) {
 		const current = nums2[i];
@@ -23,7 +23,7 @@ const intersect = function (nums1, nums2) {
 		}
 	}
 
-	return resultArray;
+	return 'Intersection: ' + resultArray ;
 };
 
 console.log(intersect(input1, input2));

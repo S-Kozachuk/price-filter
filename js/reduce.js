9d84b4add154206290fcdadf58a492d264ad2b6f
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-05-27 12:16:12 +0300

    REDUCE.JS/Studying the reduce()

--- js/reduce.js
+++ js/reduce.js
@@ -1,5 +1,19 @@
-let a = [2,5,7,1];
-let b = a.reduce(function(previousValue,item){
-	return previousValue + item;
-});
-console.log(b);
+// Cycle for
+let a = [1,2,3,4];
+let sum = 0;
+
+for (let i=0; i < a.length; i++) {
+	sum = sum + a[i];
+}
+
+console.log(sum);
+
+// Метод Reduce
+// prev - значение, вычисленное в процессе выполнения предыдущего
+// цикла (итерации).
+// item - значение элемента перебираемого массива
+
+
+let b = a.reduce(function(prev, item){
+
+})

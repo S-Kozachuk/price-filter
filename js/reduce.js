97d9d45f965ea57f0576856e32a17b2aeb6aa7ba
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-05-27 14:02:18 +0300

    REDUCE.JS/Searching max value from array

--- js/reduce.js
+++ js/reduce.js
@@ -1,4 +1,5 @@
 // Cycle for
+/*
 let a = [1,2,3,4];
 let sum = 0;
 
@@ -7,8 +8,14 @@ for (let i=0; i < a.length; i++) {
 }
 
 console.log(sum);
+*/
 
 // Метод Reduce
+// Когда использутеся Reduce
+/*
+Если нужно перебрать массив при этом вычислить некое общее
+значение то, нужно применять reduce.
+*/
 // Обязательные аргументы
 /* 
 prev - значение, вычисленное в процессе выполнения предыдущего
@@ -40,9 +47,22 @@ console.log(b);
 */
 
 // Reduce() with 3-th arguments
-
+/*
 let b = a.reduce((prev,item,index) => {
 	return prev + item + index;
 });
 
 console.log(b);
+*/
+
+// Search the max value from array
+let d = [2,5,33,51,7,8,38,40]
+let max = d[0];
+
+for (let i = 0; i < d.length; i++) {
+	if (d[i] > max) {
+		max = d[i];
+	}
+}
+
+console.log(max);

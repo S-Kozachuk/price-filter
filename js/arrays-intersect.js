06a417c62ea262492cae3b94252c1116ea4e482f
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-05-31 13:25:40 +0300

    INSCT/Changed variable map

--- js/arrays-intersect.js
+++ js/arrays-intersect.js
@@ -7,20 +7,20 @@ const input4 = [9,4,9,8,4];
 const intersect = function (nums1, nums2) {
 	let resultArray = [];
 	// Get a count of occurences in map (from nums1)
-	let map = nums1.reduce((acc,i)=>{
+	let store = nums1.reduce((acc,i)=>{
 		acc[i] = acc[i] ? acc[i] + 1 : 1;
 		return acc;
 	}, {})
 
-	console.log(map);
-
+	console.log(store);
+// Заменить map
 	// Search the intersections in 2-nd array (nums2)
 	for (let i = 0; i < nums2.length; i++) {
 		const current = nums2[i];
-		let count = map[current];
+		let count = store[current];
 		if (count && count > 0) {
 			resultArray.push(current);
-			map[current] -= 1;
+			//store[current] -= 1;
 		}
 	}
 

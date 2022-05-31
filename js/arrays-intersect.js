246b818578028cb46c82472ba73a4a5257a5d32d
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-05-31 15:50:08 +0300

    INSCT/Added the last changes

--- js/arrays-intersect.js
+++ js/arrays-intersect.js
@@ -1,7 +1,7 @@
 // const input1 = [1,2,2,1];
 // const input2 = [2,2];
 
-const input3 = [4,9,5];
+const input3 = [4,9,5,9];
 const input4 = [9,4,9,8,4];
 
 const intersect = function (nums1, nums2) {
@@ -13,6 +13,8 @@ const intersect = function (nums1, nums2) {
 	}, {})
 
 	console.log(store);
+	console.log(store[4]);
+
 // Заменить map
 	// Search the intersections in 2-nd array (nums2)
 	for (let i = 0; i < nums2.length; i++) {
@@ -20,7 +22,7 @@ const intersect = function (nums1, nums2) {
 		let count = store[current];
 		if (count && count > 0) {
 			resultArray.push(current);
-			//store[current] -= 1;
+			store[current] -= 1;
 		}
 	}
 

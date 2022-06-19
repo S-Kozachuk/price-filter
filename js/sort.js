9ba52be141e01e793c03b32e3e6373d1b6eed786
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-06-19 09:48:04 +0300

    SA/Began study sort()

--- js/sort.js
+++ js/sort.js
@@ -1,3 +1,4 @@
+/*
 const arr = [5,1,4,40,12];
 
 function compare (a, b) {
@@ -10,10 +11,19 @@ function compare (a, b) {
 	return comparison
 }
 
-/*
+
 function compare (a, b){
 	return a - b;
 }
-*/
+
 result = arr.sort(compare);
 console.log(result)
+*/
+
+var arr = [12, 7, 3, 15, 5, 134, 8, 1];
+let arr2 = arr;
+
+arr.sort();
+
+console.log(arr);
+console.log('a'>'aaa');
